9ece0fe6f68d2627684203ee43334b3f0ad396bb
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-10-31 20:21:30 +0000

    added a key to each of the message components

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: [{username: "blob", content: "blobbers about"}]
+      messages: [{username: "blob", content: "blobbers about", id:"id1"}]
     };
   }
 
