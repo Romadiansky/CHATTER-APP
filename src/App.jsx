2e562ba98cbaead7982442385abcf8e433aec366
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-10-31 02:48:42 +0000

    passed currentUser to the ChatBar component and the messages to the MessageList component, and modified both components to render this data

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,26 @@ import Message from './Message.jsx';
 import MessageList from './MessageList.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [{username: "blob", content: "blobbers about"}]
+    };
+  }
+
+  // // Called after the component was rendered and it was attached to the
+  // // DOM. This is a good place to make AJAX requests or setTimeout.
+  // componentDidMount() {
+  //   // After 3 seconds, set `loading` to false in the state.
+  //   setTimeout(() => {
+  //     this.setState({loading: false}); // this triggers a re-render!
+  //   }, 3000)
+  // }
   render() {
     return (
      <div>
-      <ChatBar/>
-      <MessageList/>
+      <ChatBar currentUser={this.state.currentUser}/>
+      <MessageList messages={this.state.messages}/>
      </div>
     );
   }
